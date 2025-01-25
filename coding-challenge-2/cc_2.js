4e0302ae9295e3977c4f317df030a041daad04c7
Aisha Nomani <aishan@usf.edu>
2025-01-25 15:27:06 -0500

    Task 3 - Employee Records

--- coding-challenge-2/cc_2.js
+++ coding-challenge-2/cc_2.js
@@ -15,3 +15,16 @@ let total = scores.reduce((sum, score) => sum + score, 0);
 let average = total / scores.length;
 console.log("Updated scores:", scores);
 console.log("Average score:", average);
+
+// Task 3: Working With Objects
+// Scenario: Employee Records
+console.log("********** Employee Records **********");
+let employee = {
+    name: "Jason Borne",
+    age: 30,
+    department: "Sales",
+    isActive: true
+};
+employee.department = "Marketing";
+employee.position = "Manager";
+console.log("Updated employee record:", employee);
