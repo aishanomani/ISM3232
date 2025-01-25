efd6020accc470473f1447600aff6ad578e06378
Aisha Nomani <aishan@usf.edu>
2025-01-24 22:33:05 -0500

    Task 1 - Employee Information.

---
+++ coding-challenge-1/cc_1.js
@@ -0,0 +1,10 @@
+// Task 1: Variable Declaration
+// Scenario: Employee Information
+
+console.log("********** Employee Information **********");
+let employeeName = "Jane Smith"; 
+console.log("Employee Name: " + employeeName + " - Variable Type: " + typeof(employeeName));  
+const employeeID = 1112223333;
+console.log("Employee ID: " + employeeID + " - Variable Type: " + typeof(employeeID));
+var isActive = true;
+console.log("Is Employee Active: " + isActive + " - Variable Type: " + typeof(isActive));
