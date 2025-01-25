a21836ad0aec7ca96802c68912e0f87db64e326d
Aisha Nomani <aishan@usf.edu>
2025-01-25 15:16:22 -0500

    Task 1 - Store Inventory

---
+++ coding-challenge-2/cc_2.js
@@ -0,0 +1,7 @@
+// Task 1: Working With Arrays
+// Scenario: Store Inventory
+console.log("********** Store Inventory **********");
+let products = ["Laptop", "Mouse", "Keyboard", "Monitor", "Printer"];
+products.push("Tablet");
+products.pop();
+console.log("Updated Product List:", products);
