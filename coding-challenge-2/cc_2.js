0b8c4904e2c8e5d7364bb361e50cd195c5134a3c
Aisha Nomani <aishan@usf.edu>
2025-01-25 15:21:19 -0500

    Task 2 - Student Scores

--- coding-challenge-2/cc_2.js
+++ coding-challenge-2/cc_2.js
@@ -5,3 +5,13 @@ let products = ["Laptop", "Mouse", "Keyboard", "Monitor", "Printer"];
 products.push("Tablet");
 products.pop();
 console.log("Updated Product List:", products);
+
+// Task 2: Accessing and Modifying Arrays
+// Scenario: Student Scores
+console.log("********** Student Scores **********");
+let scores = [85, 90, 78, 88, 92];
+scores[1] = 95;
+let total = scores.reduce((sum, score) => sum + score, 0);
+let average = total / scores.length;
+console.log("Updated scores:", scores);
+console.log("Average score:", average);
