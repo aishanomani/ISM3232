9a7d75a11a04ca217e1e5787bd4b7c0026ea7cd4
Aisha Nomani <aishan@usf.edu>
2025-01-24 22:34:21 -0500

    Task 2 - Product Details.

--- coding-challenge-1/cc_1.js
+++ coding-challenge-1/cc_1.js
@@ -8,3 +8,14 @@ const employeeID = 1112223333;
 console.log("Employee ID: " + employeeID + " - Variable Type: " + typeof(employeeID));
 var isActive = true;
 console.log("Is Employee Active: " + isActive + " - Variable Type: " + typeof(isActive));
+
+// Task 2: Primitive Data Types
+// Scenario: Product Details
+console.log("********** Product Details **********");
+let productName = "Apple MacBook"; 
+console.log("Product Name: " + productName + " - Variable Type: " + typeof(productName));  
+const productPrice = 1000;
+console.log("Product Price: $" + productPrice + " - Variable Type: " + typeof(employeeID));
+var isAvailable = true;
+console.log("Is Product Available: " + isAvailable + " - Variable Type: " + typeof(isAvailable));
+
