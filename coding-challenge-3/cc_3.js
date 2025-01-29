41c3fbe471469102c6e56adf5288bde45ad2ffe0
Aisha Nomani <aishan@usf.edu>
2025-01-28 19:55:03 -0500

    Task 1 - Product Price Management

---
+++ coding-challenge-3/cc_3.js
@@ -0,0 +1,7 @@
+// Task 1: Product Price Management
+// Scenario: Online Store
+console.log("********** Online Store **********");
+let prices = [9.99, 19.99, 29.99, 39.99, 49.99];
+prices.push(59.99);
+prices.shift();
+console.log("Updated Prices:", prices);
