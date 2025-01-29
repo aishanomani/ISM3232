90f378c8f112532ab138cc46abab35854a90fa29
Aisha Nomani <aishan@usf.edu>
2025-01-28 19:58:39 -0500

    Task 2 - Modifying Customer Orders

--- coding-challenge-3/cc_3.js
+++ coding-challenge-3/cc_3.js
@@ -5,3 +5,12 @@ let prices = [9.99, 19.99, 29.99, 39.99, 49.99];
 prices.push(59.99);
 prices.shift();
 console.log("Updated Prices:", prices);
+
+// Task 2: Modifying Customer Orders
+// Scenario: Order List
+console.log("********** Order List **********");
+let orders = [10, 20, 30, 40, 50];
+orders[2] += 5;
+let totalOrders = orders.reduce((total, quantity) => total + quantity, 0);
+console.log("Updated Orders:", orders);
+console.log("Total Order Count:", totalOrders);
