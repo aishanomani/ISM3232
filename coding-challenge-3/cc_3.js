b01f72b4ca7ace09e26c4e6f41dc443381d48662
Aisha Nomani <aishan@usf.edu>
2025-01-28 20:08:28 -0500

    Task 3 - Employee Performance Tracking

--- coding-challenge-3/cc_3.js
+++ coding-challenge-3/cc_3.js
@@ -14,3 +14,16 @@ orders[2] += 5;
 let totalOrders = orders.reduce((total, quantity) => total + quantity, 0);
 console.log("Updated Orders:", orders);
 console.log("Total Order Count:", totalOrders);
+
+// Task 3: Employee Performance Tracking
+// Scenario: HR System
+console.log("********** HR System **********");
+const employee = {
+    name: "Jingyuan Chen",
+    role: "Business Analyst",
+    performanceScore: 94,
+    isActive: true,
+};
+employee.performanceScore = 99;
+employee.promotionEligible = true;
+console.log("Updated Employee Object:", employee);
