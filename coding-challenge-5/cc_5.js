bde44f630c4c63d43fe73cd0256b4a5b31e3183b
Aisha Nomani <aishan@usf.edu>
2025-02-04 12:30:39 -0500

    Task 2 - Order Details

--- coding-challenge-5/cc_5.js
+++ coding-challenge-5/cc_5.js
@@ -6,3 +6,17 @@ console.log(`Name: ${customer.name}`);
 console.log(`Age: ${customer.age}`);
 console.log(`Email: ${customer.email}`);
 
+// Task 2: Object Methods
+// Scenario: Order Details
+console.log("********** Order Details **********");
+const order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder() {
+      console.log(`Order ID: ${this.orderId}, Total Amount: $${this.totalAmount}, Status: ${this.status}`);
+    }
+  };
+  order.displayOrder();
+
+  
