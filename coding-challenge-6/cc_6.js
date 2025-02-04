4cbc4e5c8400625e1f178df33141ef3a458669b3
Aisha Nomani <aishan@usf.edu>
2025-02-04 14:37:07 -0500

    Task 2 - Sales Tax Computation

--- coding-challenge-6/cc_6.js
+++ coding-challenge-6/cc_6.js
@@ -7,3 +7,14 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 }
 console.log(calculateProfit(20, 30, 100)); // Expected output: "Total Profit: $1000"
 console.log(calculateProfit(50, 70, 200)); // Expected output: "Total Profit: $4000"
+
+// Task 2: Function Expression
+// Scenario: Sales Tax Computation
+console.log("********** Sales Tax Computation **********");
+const calculateSalesTax = function(amount, taxRate) {
+    let tax = amount * taxRate;
+    return `Sales Tax: $${tax}`;
+};
+console.log(calculateSalesTax(100, 0.07)); // Expected output: "Sales Tax: $7"
+console.log(calculateSalesTax(500, 0.1));  // Expected output: "Sales Tax: $50"
+
