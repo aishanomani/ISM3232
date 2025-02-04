ab9e193c866fd06550fdcbac2ee5a3ff3f2a356d
Aisha Nomani <aishan@usf.edu>
2025-02-04 14:12:30 -0500

    Task 1 - Business Profit Calculation

---
+++ coding-challenge-6/cc_6.js
@@ -0,0 +1,9 @@
+// Task 1: Function Declaration
+// Scenario: Business Profit Calculation
+console.log("********** Business Profit Calculation **********");
+function calculateProfit(costPrice, sellingPrice, unitsSold) {
+    let profit = (sellingPrice - costPrice) * unitsSold;
+    return `Total Profit: $${profit}`;
+}
+console.log(calculateProfit(20, 30, 100)); // Expected output: "Total Profit: $1000"
+console.log(calculateProfit(50, 70, 200)); // Expected output: "Total Profit: $4000"
