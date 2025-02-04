b238522325eb7b9aa711b5555565806134c8b498
Aisha Nomani <aishan@usf.edu>
2025-02-04 12:27:55 -0500

    Task 1 - Customer Profile

---
+++ coding-challenge-5/cc_5.js
@@ -0,0 +1,8 @@
+// Task 1: Object Properties
+// Scenario: Customer Profile
+console.log("********** Customer Profile **********");
+const customer = { name: "John Doe", age: 35, email: "john.doe@email.com" };
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
+
