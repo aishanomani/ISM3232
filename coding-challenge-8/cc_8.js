9222502dbb88bb608c36c66a1d5284e935636833
Aisha Nomani <aishan@usf.edu>
2025-02-11 20:23:12 -0500

    Task 5 - Loan Payment Calculation

--- coding-challenge-8/cc_8.js
+++ coding-challenge-8/cc_8.js
@@ -40,3 +40,12 @@ function calculateRentalCost(days, carType, insurance = false) {
 console.log(calculateRentalCost(3, "Economy", true)); // "Total Rental Cost: $180"
 console.log(calculateRentalCost(5, "Luxury", false)); // "Total Rental Cost: $500"
 
+// Task 5: Returning Values
+// Scenario: Loan Payment Calculation
+console.log("********** Loan Payment Calculation **********");
+function calculateLoanPayment(principal, rate, time) {
+    let totalPayment = principal + (principal * rate * time);
+    return `Total Payment: $${totalPayment.toFixed(2)}`;
+}
+console.log(calculateLoanPayment(1000, 0.05, 2)); // "Total Payment: $1100.00"
+console.log(calculateLoanPayment(5000, 0.07, 3)); // "Total Payment: $6050.00"
