160d2a82eee0536ffcd2b6e955800af448c993b8
Aisha Nomani <aishan@usf.edu>
2025-02-11 20:24:30 -0500

    Task 6 - Identifying Large Transactions

--- coding-challenge-8/cc_8.js
+++ coding-challenge-8/cc_8.js
@@ -49,3 +49,13 @@ function calculateLoanPayment(principal, rate, time) {
 }
 console.log(calculateLoanPayment(1000, 0.05, 2)); // "Total Payment: $1100.00"
 console.log(calculateLoanPayment(5000, 0.07, 3)); // "Total Payment: $6050.00"
+
+// Task 6: Higher-Order Functions
+// Scenario: Identifying Large Transactions
+console.log("********** Identifying Large Transactions **********");
+let transactions = [200, 1500, 3200, 800, 2500];
+function filterLargeTransactions(transactions, filterFunction) {
+    return transactions.filter(filterFunction);
+}
+console.log(filterLargeTransactions(transactions, amount => amount > 1000)); // [1500, 3200, 2500]
+
